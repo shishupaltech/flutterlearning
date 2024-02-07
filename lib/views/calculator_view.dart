bdf7e6d59329d3431e4854835d9c8bdf3eafb9c2
shishupaltech <veerutech75@gmail.com>
2024-02-07 18:03:04 +0530

    button and inputs maked

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorVIEW extends StatelessWidget {
@@ -6,35 +7,75 @@ class CalculatorVIEW extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(32.0),
+      padding: EdgeInsets.all(32.0),
       child: Column(
         children: [
           // calculator display
-          TextField(
-            keyboardType: TextInputType.number,
-            autofocus: true,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.white,
-                  ),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(30)),
-                labelText: "0",
-                hintText: "Enter a number",
-                hintStyle: const TextStyle(
-                  color: Colors.white,
-                )),
+          const DisplayOne(hint: "Enter First Number"),
+          const SizedBox(
+            height: 30,
           ),
+          const DisplayOne(hint: "Enter Second Number"),
+          const Text(
+            "0",
+            style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+          ),
+          const Spacer(),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(CupertinoIcons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(CupertinoIcons.minus),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(CupertinoIcons.multiply),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(CupertinoIcons.divide),
+              ),
+            ],
+          )
           // expand
           // button
         ],
       ),
+    );
+  }
+}
+
+class DisplayOne extends StatelessWidget {
+  const DisplayOne({super.key, this.hint = "Enter a Number"});
+  final String? hint;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      autofocus: true,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.white,
+            ),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Colors.white,
+              ),
+              borderRadius: BorderRadius.circular(30)),
+          labelText: "0",
+          hintText: hint,
+          hintStyle: const TextStyle(
+            color: Colors.white,
+          )),
     );
   }
 }
